e066e77fd5fb9736aaf61a7e8f30777d0825720c
himanshupatel2007 <himanshupatel.942007@gmail.com>
2025-12-06 12:37:25 +0530

    done

--- BgColorChanger/script.js
+++ BgColorChanger/script.js
@@ -1,34 +1,81 @@
 
-let value = styles = index = ""
+let value = last = current = color = passing = passed =  ""
 let themeclass = document.querySelector("#themetoggle")
 let buttons = document.querySelectorAll("button")
 let favourite = document.querySelector(".setfavourite")
 let applyfavourite = document.querySelector(".applyfavourite")
+let currentcolor = document.querySelector("#currentcolor")
+let lastcolor = document.querySelector("#lastcolor")
+let randombutton = document.querySelector(".randomstyle")
 
-function identifying(value) {
 
+let arr = [
+    {
+        id : "one.css" ,value :0 
+    },
+    {
+        id : "two.css" ,value :1
+    },
+    {
+        id : "three.css" ,value :2
+    },
+    {
+        id : "four.css" ,value :3 
+    },
+    {
+        id : "five.css" ,value :4
+    },
+    {
+        id : "six.css" ,value :5
+    },
+    {
+        id : "seven.css" ,value :6
+    }
+]
+
+
+function identifying(value,color) {
+    last = current;
+    current = color
     themeclass.href = value;
     favourite.classList.remove("hideit")
-    if (localStorage.getItem("fav")) 
-        applyfavourite.classList.remove("hideit")
-
+    lastcolor.textContent = `Last Color : ${last}`
+    currentcolor.textContent = `Current Color : ${current}`
+    buttons.forEach(element => {
+        if(element.classList.contains(value))
+            element.classList.add("clicked")
+        else
+            element.classList.remove("clicked")
+        
+    });
 }
 
+
 favourite.addEventListener("click", () => {
-    localStorage.setItem("fav", themeclass.href)
+    localStorage.setItem("fav", themeclass.href);
+    if (localStorage.getItem("fav")) 
+        applyfavourite.classList.remove("hideit")
 })
 
 
 applyfavourite.addEventListener("click", () => {
-
-    let value = localStorage.getItem("fav")
-    themeclass.href = value;
-
+    
+    let savedvalue = localStorage.getItem("fav")
+    themeclass.href = savedvalue;
 })
 
 
-window.onload = () => {
-    let saved = LocalStorage.getItem("fav")
-    if (saved) 
-        applyfavourite.classList.remove("hideit")
-}
+
+randombutton.addEventListener("click", ()=>{
+    let random = Math.floor(Math.random() * 7)
+    arr.forEach(e=>{
+        if(e.value == random)
+          passed = e.id;
+    })
+    let classes ;
+    buttons.forEach(e =>{
+      if(e.classList.contains(passed))
+        classes = e.textContent
+    })
+    identifying(passed,classes)
+})
